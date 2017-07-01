80116123a94c475b33929903548ee4596c4819d9
Andrew Zigerelli <andrewzigerelli@users.noreply.github.com>
2017-07-01 16:20:02 -0400

    updated profile script; had minor errors

--- profile/cleanup.sh
+++ profile/cleanup.sh
@@ -2,3 +2,4 @@
 rm *.out
 rm *.pgm
 rm *.txt
+rm *_output
--- profile/profile.sh
+++ profile/profile.sh
@@ -11,9 +11,9 @@ $NVPROF $OPTIONS ../parboil/bin/histo 20 4 -i ../parboil/datasets/histo/default/
 $NVPROF $OPTIONS ../parboil/bin/lbm 100 -i ../parboil/datasets/lbm/short/input/120_120_150_ldc.of -o lbm.out 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../parboil/bin/mri-gridding 32 0 -i ../parboil/datasets/mri-gridding/small/input/small.uks -o mri-gridding.out 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../parboil/bin/mri-q -i ../parboil/datasets/mri-q/small/input/32_32_32_dataset.bin -o mri-q.out 2>> $OUTPUT_FILE
-$NVPROF $OPTIONS ../parboil/bin/sad -i ../parboil/datasets/sad/default/input/frame.bin../parboil/datasets/sad/default/input/reference.bin -o sad.out 2>> $OUTPUT_FILE
-$NVPROF $OPTIONS ../parboil/bin/sgemm -i ../parboil/datasets/sgemm/small/input/matrix1.txt../parboil/datasets/sgemm/small/input/matrix2.txt../parboil/datasets/sgemm/small/input/matrix2t.txt -o sgemm.out 2>> $OUTPUT_FILE
-$NVPROF $OPTIONS ../parboil/bin/spmv -i ../parboil/datasets/spmv/small/input/1138_bus.mtx../parboil/datasets/spmv/small/input/vector.bin -o spmv.out 2>> $OUTPUT_FILE
+$NVPROF $OPTIONS ../parboil/bin/sad -i ../parboil/datasets/sad/default/input/frame.bin ../parboil/datasets/sad/default/input/reference.bin -o sad.out 2>> $OUTPUT_FILE
+$NVPROF $OPTIONS ../parboil/bin/sgemm -i ../parboil/datasets/sgemm/small/input/matrix1.txt ../parboil/datasets/sgemm/small/input/matrix2.txt ../parboil/datasets/sgemm/small/input/matrix2t.txt -o sgemm.out 2>> $OUTPUT_FILE
+$NVPROF $OPTIONS ../parboil/bin/spmv -i ../parboil/datasets/spmv/small/input/1138_bus.mtx ../parboil/datasets/spmv/small/input/vector.bin -o spmv.out 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../parboil/bin/stencil 128 128 32 100 -i ../parboil/datasets/stencil/small/input/128x128x32.bin -o stencil.out 2>> $OUTPUT_FILE
 #rodinia
 $NVPROF $OPTIONS ../rodinia_3.1/bin/backprop 256  2>> $OUTPUT_FILE
@@ -27,7 +27,7 @@ $NVPROF $OPTIONS ../rodinia_3.1/bin/leukocyte  ../rodinia_3.1/data/leukocyte/tes
 $NVPROF $OPTIONS ../rodinia_3.1/bin/lud_cuda -s 256 -v 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../rodinia_3.1/bin/mummergpu ../rodinia_3.1/data/mummergpu/NC_003997.fna ../rodinia_3.1/data/mummergpu/NC_003997_q100bp.fna 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../rodinia_3.1/bin/needle 2048 10 2>> $OUTPUT_FILE
-$NVPROF $OPTIONS ../rodinia_3.1/bin/nn../rodinia_3.1/cuda/nn/filelist_4 -r 5 -lat 30 -lng 90  2>> $OUTPUT_FILE
+$NVPROF $OPTIONS ../rodinia_3.1/bin/nn ../rodinia_3.1/cuda/nn/filelist_4 -r 5 -lat 30 -lng 90  2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../rodinia_3.1/bin/particlefilter_float -x 128 -y 128 -z 10 -np 1000 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../rodinia_3.1/bin/particlefilter_naive -x 128 -y 128 -z 10 -np 1000 2>> $OUTPUT_FILE
 $NVPROF $OPTIONS ../rodinia_3.1/bin/pathfinder 100000 100 20  2>> $OUTPUT_FILE
